bf01c6975327d46694adc3c23a5608cc7ba1ff61
nikkoberse <nikko.berse.23@gmail.com>
2023-02-23 16:00:25 +0800

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage"
+import Products from "./ProductsPage"
+import SingleProduct from "./SingleProductPage"
+import About from "./AboutPage"
+import Checkout from "./CheckoutPage"
+import Cart from "./CartPage"
+import PrivateRoute from "./PrivateRoute"
+import Error from "./ErrorPage"
+import AuthWrapper from "./AuthWrapper"
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Checkout,
+  Cart,
+  PrivateRoute,
+  Error,
+  AuthWrapper,
+}
